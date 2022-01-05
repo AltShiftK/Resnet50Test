34a01a1554bed59f5c854e8488825daeed52c2b4
AltShiftK <door047@gmail.com>
2022-01-04 22:02:16 -0600

    Update ViewController.swift
    Added label functionality to make app work without console

--- ViewController.swift
+++ ViewController.swift
@@ -12,9 +12,16 @@ import Vision
 
 class ViewController: UIViewController, AVCaptureVideoDataOutputSampleBufferDelegate {
 
+    let identifierLabel: UILabel = {
+        let label = UILabel()
+        label.backgroundColor = .white
+        label.textAlignment = .center
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        
         // Startup Camera
         print("test")
         let captureSession = AVCaptureSession()
@@ -30,40 +37,62 @@ class ViewController: UIViewController, AVCaptureVideoDataOutputSampleBufferDele
         catch {
             print("Error")
         }
-        
         captureSession.startRunning()
-        
+
         // Make camera visible
-        let previewLayer = AVCaptureVideoPreviewLayer(session: captureSession)
+        let previewLayer = AVCaptureVideoPreviewLayer(session: captureSession) as AVCaptureVideoPreviewLayer
+
+        previewLayer.frame = view.bounds
         view.layer.addSublayer(previewLayer)
-        previewLayer.frame = view.frame
-        
+
+        // Make overlays visible
+        var previewView = UIView(frame: view.frame)
+        view.addSubview(previewView)
+        previewView.layer.addSublayer(previewLayer)
+
+
+        let subButton = UIButton()
+        subButton.setBackgroundImage(UIImage(named: "capImage"), for: UIControl.State.normal)
+        subButton.frame = CGRect(x: 15, y: 50, width: 60, height: 60)
+        subButton.center.x = view.center.x
+        self.view.addSubview(subButton)
+
         // Make an output from the camera
         let dataOutput = AVCaptureVideoDataOutput()
         dataOutput.setSampleBufferDelegate(self, queue: DispatchQueue(label: "videoQueue"))
         captureSession.addOutput(dataOutput)
-
+        
+        setupIdentifierConfidenceLabel()
+    }
+    
+    fileprivate func setupIdentifierConfidenceLabel(){
+        view.addSubview(identifierLabel)
+        identifierLabel.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: -32).isActive = true
+        identifierLabel.leftAnchor.constraint(equalTo: view.leftAnchor).isActive = true
+        identifierLabel.rightAnchor.constraint(equalTo: view.rightAnchor).isActive = true
+        identifierLabel.heightAnchor.constraint(equalToConstant: 40).isActive = true
     }
 
     func captureOutput(_ output: AVCaptureOutput, didOutput sampleBuffer: CMSampleBuffer, from connection: AVCaptureConnection) {
         
+        //print("Camera was able to capture", Date())
         guard let pixelBuffer: CVPixelBuffer = CMSampleBufferGetImageBuffer(sampleBuffer) else { return }
-        
         guard let model = try? VNCoreMLModel(for: Resnet50().model) else { return }
-        
         let request = VNCoreMLRequest(model: model) { (finishedReq, err) in
             // print(finishedReq.results)
             // finishedReq.results is raw result data (unusable)
-                                                     
             guard let results = finishedReq.results as? [VNClassificationObservation] else { return }
-            
             guard let firstObservation = results.first else { return }
-            
             print(firstObservation.identifier, firstObservation.confidence)
+            
+            DispatchQueue.main.async {
+                self.identifierLabel.text = "\(firstObservation.identifier) \(firstObservation.confidence * 100)%"
+            }
+            
         }
-        
         // Sends request to the algorithm (Resnet50)
         try? VNImageRequestHandler(cvPixelBuffer: pixelBuffer, options: [:]).perform([request])
     }
+
 }
 
